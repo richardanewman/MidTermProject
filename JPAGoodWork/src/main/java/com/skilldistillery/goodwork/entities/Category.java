4c1d46ec468dbc0e581d1411456a522d54d10f41
David Norris <Norris.David.Dev@gmail.com>
2019-11-15 15:15:00 -0700

    Finishing mapping on all the entities

--- JPAGoodWork/src/main/java/com/skilldistillery/goodwork/entities/Category.java
+++ JPAGoodWork/src/main/java/com/skilldistillery/goodwork/entities/Category.java
@@ -3,6 +3,7 @@ package com.skilldistillery.goodwork.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,7 +19,7 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
-	@ManyToMany
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
 	@JoinTable(name = "event_has_category", joinColumns = @JoinColumn(name = "category_id"), inverseJoinColumns = @JoinColumn(name = "event_id"))
 	private List<Event> events;
 
