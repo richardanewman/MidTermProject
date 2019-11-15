846967d21c575d5fdd80a7b39abe9e77c000f29e
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-15 11:01:00 -0700

    locationEntity

---
+++ JPAGoodWork/src/main/java/com/skilldistillery/goodwork/entities/location.java
@@ -0,0 +1,132 @@
+package com.skilldistillery.goodwork.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class location {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String address;
+
+	private String address2;
+
+	private String city;
+
+	private String state;
+
+	@Column(name = "zip_code")
+	private int zipCode;
+	
+	
+
+	public location() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public String toString() {
+		return "location [id=" + id + ", address=" + address + ", address2=" + address2 + ", city=" + city + ", state="
+				+ state + ", zipCode=" + zipCode + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((address2 == null) ? 0 : address2.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + zipCode;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		location other = (location) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (address2 == null) {
+			if (other.address2 != null)
+				return false;
+		} else if (!address2.equals(other.address2))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (id != other.id)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (zipCode != other.zipCode)
+			return false;
+		return true;
+	}
+
+}
