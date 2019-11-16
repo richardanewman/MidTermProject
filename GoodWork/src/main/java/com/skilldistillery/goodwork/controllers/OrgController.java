c2f2c71c4f5e6695fe061bf78502fff805c8f978
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-11-16 11:31:07 -0700

    adds getAll and findById methods to orgDAO and displays them to appropriate jsp files

--- GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/OrgController.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/OrgController.java
@@ -1,5 +1,43 @@
 package com.skilldistillery.goodwork.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.goodwork.data.OrgDAO;
+import com.skilldistillery.goodwork.entities.Organization;
+
+@Controller
 public class OrgController {
+	
+	@Autowired
+	private OrgDAO orgDAO;
+	
+		
+	@RequestMapping(path="getAllOrgs.do", method = RequestMethod.GET)
+	public String diplayAllOrgs(Model model) {
+		List<Organization> organizationList = orgDAO.getAll();
+		model.addAttribute("displayAll", organizationList);
+		
+		return "result";
+		
+	}
+	
+	@RequestMapping(path="findById.do", method = RequestMethod.GET)
+	public String diplayOrg(int id, Model model) {
+		if (orgDAO.findById(id) == null) {
+			model.addAttribute("oops", "Looks like something went wrong. Please check your ID number and try again.");
+			return "fail";
+		}else {
+			
+		model.addAttribute("org", orgDAO.findById(id));
+		
+		return "result";
+		}
+	}
 
 }
--- GoodWork/src/main/webapp/WEB-INF/result.jsp
+++ GoodWork/src/main/webapp/WEB-INF/result.jsp
@@ -1,12 +1,39 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Result</title>
 </head>
 <body>
+<div class="displayResults">
+<c:if test="${! empty displayAll}">
+<h1>All Organizations:</h1>
+<c:forEach items="${displayAll}" var="org">
+${org.orgName}<br><br>
+${org.location.address}<br><br>
+${org.location.city}<br><br>
+${org.location.state}<br><br>
+${org.orgType}<br><br>
+${org.orgNum}<br><br>
+${org.logoURL}<br><br>
+${org.website}<br><br>
+</c:forEach>
+</c:if>
 
+
+<c:if test="${! empty org}">
+<h1>Found it!</h1>
+${org.orgName}<br><br>
+${org.location.address}<br><br>
+${org.location.city}<br><br>
+${org.location.state}<br><br>
+${org.orgType}<br><br>
+${org.orgNum}<br><br>
+${org.logoURL}<br><br>
+${org.website}<br><br>
+</c:if>
 </body>
 </html>
