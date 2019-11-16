6fab023ecb2a5c3be9db24a7fce4ae4f35a99d1b
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-16 10:39:48 -0700

    eventCont commit

--- GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
@@ -2,7 +2,11 @@ package com.skilldistillery.goodwork.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.skilldistillery.goodwork.data.EventDAO;
 
 @Controller
 public class EventController {
@@ -10,6 +14,16 @@ public class EventController {
 	@Autowired
 	private EventDAO eventDAO;
 	
-	@RequestMapping(path="getEvent" method = RequestMethod.GET)
-	public String 
+	@RequestMapping(path="getEvent.do", method = RequestMethod.GET)
+	public String getEvent(Model model) {
+		model.addAttribute("id", eventDAO.);
+		return "event";
+		
+	}
+	
+	@RequestMapping(path="getEventList.do", method = RequestMethod.GET)
+	public String eventList(Model model) {
+		model.addAttribute("eventList", eventDAO.);
+		return 
+	}
 }
