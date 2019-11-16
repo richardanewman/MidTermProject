76353f76c63aa502e23509690093bcd567e6fb13
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-16 10:26:55 -0700

    intitial

--- GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
@@ -1,8 +1,15 @@
 package com.skilldistillery.goodwork.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EventController {
-
+	
+	@Autowired
+	private EventDAO eventDAO;
+	
+	@RequestMapping(path="getEvent" method = RequestMethod.GET)
+	public String 
 }
