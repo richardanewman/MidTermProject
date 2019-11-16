27ed17493f2f9bee26d4495e1b3c76fe6b0ef306
David Norris <Norris.David.Dev@gmail.com>
2019-11-16 12:41:36 -0700

    Adding functionality to authdao and controllers

--- GoodWork/src/main/java/com/skilldistillery/goodwork/data/AuthDAOImpl.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/data/AuthDAOImpl.java
@@ -1,5 +1,63 @@
 package com.skilldistillery.goodwork.data;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.goodwork.entities.User;
+
+@Transactional
+@Service
 public class AuthDAOImpl implements AuthDAO {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public boolean validUserName(String userName) {
+		List<User> users = null;
+		String sql = "SELECT user FROM User user WHERE user.userName = :uName";
+		users = em.createQuery(sql, User.class).setParameter("uName", userName).getResultList();
+		if (users != null) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean validPassword(String password) {
+		List<User> users = null;
+		String sql = "SELECT user FROM User user WHERE user.password = :uPass";
+		users = em.createQuery(sql, User.class).setParameter("uPass", password).getResultList();
+		if (users != null) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public User loginUser(String userName, String password) {
+		User u = null;
+		List<User> user = null;
+		String sql = "SELECT user FROM User user WHERE user.userName = :uName AND user.password = :uPass";
+		user = em.createQuery(sql, User.class).setParameter("uName", userName).setParameter("uPass", password).getResultList();
+		
+		if(user != null && user.size() < 2) {
+			u = user.get(0);
+			return u;
+		}
+		return u;
+	}
+
+	@Override
+	public User registerUser(User user) {
+		em.persist(user);
+		em.flush();
+		return user;
+	}
 
 }
