6c74b24e3450bc8ffd432790934765e527ac4cac
David Norris <Norris.David.Dev@gmail.com>
2019-11-17 13:47:07 -0700

    Pushing changes to user and auth to support session

--- GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/UserController.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.goodwork.controllers;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,26 +22,30 @@ public class UserController {
 	private AuthDAO auth;
 	
 	@RequestMapping(path="updateUserForm.do", method = RequestMethod.GET)
-	public String updateUserGet(@Valid User newUser, Model model) {
-		model.addAttribute("newUser", dao.getUserById(newUser.getId()));
-		return "updateUserForm";
+	public String updateUserGet(Model model, HttpSession session) {
+		User old = (User) session.getAttribute("newUser");
+		model.addAttribute("user", old);
+		return "userJSP/updateUserForm";
 	}
 	
 	@RequestMapping(path="updateUser.do", method = RequestMethod.POST)
-	public String updateUserPost(@Valid User newUser, Model model) {
-		System.out.println(newUser);
-		model.addAttribute("newUser", dao.updateUser(newUser));
-		return "profile";
+	public String updateUserPost(@Valid User user, Model model, HttpSession session) {
+		System.out.println(user);
+		session.setAttribute("newUser", dao.updateUser(user));
+		model.addAttribute("user", new User());
+		return "userJSP/profile";
 	}
 	
 	@RequestMapping(path="disableUser.do", method = RequestMethod.POST)
-	public String deleteUser(@Valid User newUser, Model model) {
-		if(auth.validPassword(newUser.getId(), newUser.getPassword())) {
-			dao.deleteUser(newUser.getId());
+	public String deleteUser(Model model, HttpSession session) {
+		User delU = (User) session.getAttribute("newUser");
+		if(auth.validPassword(delU.getId(), delU.getPassword())) {
+			dao.deleteUser(delU.getId());
+			session.removeAttribute("newUser");
 			return "index";
 		}
-		newUser = dao.getUserById(newUser.getId());
-		model.addAttribute("newUser", newUser);
-		return "profile";
+		delU = dao.getUserById(delU.getId());
+		model.addAttribute("user", new User());
+		return "userJSP/profile";
 	}
 }
