5e3b19c8304450c98edb06b8c1f4b6ebb8f851a3
Jerry Rogers <jerryrogers@Students-MacBook-Pro.local>
2019-11-21 15:48:42 -0700

    awsloc

--- GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
+++ GoodWork/src/main/java/com/skilldistillery/goodwork/controllers/EventController.java
@@ -31,7 +31,7 @@ public class EventController {
 	private MessageDAO messDAO;
 //	private int eventId;
 
-	@RequestMapping(path = {"/","index.do"}, method = RequestMethod.GET)
+	@RequestMapping(path = {"/" , "index.do"}, method = RequestMethod.GET)
 	public String index(Model model) {
 		return "index";
 	}
